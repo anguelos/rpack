3246fb1521700f8055eb21070c2f82f48712dd9f
moeb <bmoellering@googlemail.com>
2014-05-15 14:36:25 +0200

    rpack may become installed now

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name="rpack",
+      version="0.1",
+      description="rectangle packer",
+      author="Birger Möllering",
+      author_email="bmoellering@googlemail.com",
+      url="https://github.com/moeb/rpack",
+      packages=['rpack'])
