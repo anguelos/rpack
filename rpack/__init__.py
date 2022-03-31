b5c72bf8438409b4df11cb817f680912284a4263
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2022-04-01 01:30:41 +0200

    works with python 3.8

--- rpack/__init__.py
+++ rpack/__init__.py
@@ -1,8 +1,8 @@
-from packer import Rect
-from packer import pack
-from packer import coverage
-from packer import get_enclosing_rect
-from packer import rect_cmp1
-from packer import rect_cmp2
-from packer import rect_cmp3
+from .packer import Rect
+from .packer import pack
+from .packer import coverage
+from .packer import get_enclosing_rect
+from .packer import rect_cmp1
+from .packer import rect_cmp2
+from .packer import rect_cmp3
 
